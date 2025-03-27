9d0e07152cacba16b838e84e7eaa02bcd9cdd5a9
naimislamm <naimmislam@gmail.com>
2025-03-27 12:31:07 +0600

    added some css style

--- module33.js
+++ module33.js
@@ -28,7 +28,13 @@ function displayUser(data) {
 
     // div baniye seta style kore add korbo & dynamic data bosabo----
     const div = document.createElement("div");
+    // classlist add korlam css likhew style kora jabe------
     div.classList.add("use-div");
+    // abr js er mddhew css lekha jabe--------
+    div.style.fontSize = "20px";
+    div.style.display = "flex";
+    div.style.alignItems = "center";
+    div.style.gap = "20px";
     div.innerHTML = `<h4>User:${user.name}</h4>
     <h5>Name:${user.username}</h5>
     <p>Address:${user.address.city}</p>`;
